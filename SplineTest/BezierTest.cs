887792fe8267dd58e69eb65defe69dcefee87539
m-nito <masamisce@gmail.com>
2019-07-25 03:04:15 +0900

    add scratch impl.

--- SplineTest/BezierTest.cs
+++ SplineTest/BezierTest.cs
@@ -11,17 +11,37 @@ namespace SplineTest
 {
     public class CurveFactory
     {
-        public static Bezier CreateTestBezier()
+        public static IEnumerable<Vector2> GetVectors(int cnt)
         {
-            var pts = Enumerable.Range(0, 4).Select(i => {
+            for (int i = 0; i < cnt; i++)
+            {
                 var y =
                     i == 1 ? 5 :
                     i == 2 ? 3 :
                     1;
-                return new Vector2(i, y);
-            }).ToArray();
+                yield return new Vector2(i, y);
+            }
+        }
+        public static Bezier CreateTestBezier()
+        {
+            var pts = GetVectors(4).ToArray();
             var x = new Bezier(pts[0], pts[1], pts[2], pts[3]);
             return x;
+        }
+
+        public static Vector2 GetBezierValueAt(float t)
+        {
+            var pts = GetVectors(4).ToArray();
+            return new Vector2
+            {
+                X = (float)Solve(pts[0].X, pts[1].X, pts[2].X, pts[3].X, t),
+                Y = (float)Solve(pts[0].Y, pts[1].Y, pts[2].Y, pts[3].Y, t)
+            };
+            
+        }
+        public static double Solve(float p0, float p1, float p2, float p3, float t)
+        {
+            return Math.Pow(1 - t, 3f) * p0 + 3 * Math.Pow(1 - t, 2f) * t * p1 + 3 * (1 - t) * Math.Pow(t, 2) * p2 + Math.Pow(t, 3f) * p3;
         }
     }
     public class Test
@@ -43,11 +63,22 @@ namespace SplineTest
                     bar += "|";
                     cnt += 0.1f;
                 }
-                Console.WriteLine($"{string.Format("{0:0.0}",y.X)}:{bar}");
+                Console.WriteLine($"{string.Format("{0:0.0}",y.X)}:{bar}:({y.X}:{y.Y})");
             }
 
+            for (float i = 0f; i < 1f; i += 0.05f)
+            {
+                var pos = CurveFactory.GetBezierValueAt(i);
+                string bar = "|";
+                var cnt = 0f;
+                while (cnt < pos.Y)
+                {
+                    bar += "|";
+                    cnt += 0.1f;
+                }
+                Console.WriteLine($"{string.Format("{0:0.0}", pos.X)}:{bar}({pos.X}:{pos.Y})");
+            }
             Assert.IsNotNull(curve);
         }
-
     }
 }
