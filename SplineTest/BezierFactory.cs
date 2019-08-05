f380c9ba6d03f987756ee844f1f31ea89a45b84f
m-nito <masamisce@gmail.com>
2019-08-05 09:26:50 +0900

    コード整理およびコメントを追加

---
+++ SplineTest/BezierFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Geometry;
+using System.Numerics;
+
+namespace BezierTest
+{
+    public class BezierFactory
+    {
+        /// <summary>
+        /// テスト用ベクトルのコレクションを作成し返します。
+        /// </summary>
+        /// <param name="cnt"></param>
+        /// <returns></returns>
+        public static IEnumerable<Vector2> GetVectors(int cnt)
+        {
+            // i = x としてベクトルx,yをcntの数だけ生成する
+            for (int i = 0; i < cnt; i++)
+            {
+                var y =
+                    // x = 1 のとき y = 0
+                    i == 1 ? 0 :
+                    // x = 2 のとき y = 3
+                    i == 2 ? 3 :
+                    // それ以外のとき y = 1
+                    1;
+                // 二次元ベクトルとして返す
+                yield return new Vector2(i, y);
+            }
+        }
+        /// <summary>
+        /// テスト用ベクトルを使用し、Bezierライブラリを利用したベジェ曲線を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static Bezier GetTestBezierWithLib()
+        {
+            var pts = GetVectors(4).ToArray();
+            var x = new Bezier(pts[0], pts[1], pts[2], pts[3]);
+            return x;
+        }
+
+        /// <summary>
+        /// テスト用ベクトルを使用し、基本的な公式から導いたベジェ補間値を返します。
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static Vector2 GetTestBezierValueAt(float t)
+        {
+            var pts = GetVectors(4).ToArray();
+            return new Vector2
+            {
+                X = (float)Solve(pts[0].X, pts[1].X, pts[2].X, pts[3].X, t),
+                Y = (float)Solve(pts[0].Y, pts[1].Y, pts[2].Y, pts[3].Y, t)
+            };
+            
+        }
+        /// <summary>
+        /// 地点tにおけるベジェ曲線の補間値を返します。
+        /// </summary>
+        /// <param name="p0"></param>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <param name="p3"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        private static double Solve(float p0, float p1, float p2, float p3, float t)
+        {
+            return Math.Pow(1 - t, 3f) * p0 + 3 * Math.Pow(1 - t, 2f) * t * p1 + 3 * (1 - t) * Math.Pow(t, 2) * p2 + Math.Pow(t, 3f) * p3;
+        }
+    }
+}
--- SplineTest/BezierTest.cs
+++ SplineTest/BezierTest.cs
@@ -1,84 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
-using System.Geometry;
-using System.Numerics;
 using NUnit.Framework;
 
-namespace SplineTest
+namespace BezierTest
 {
-    public class CurveFactory
-    {
-        public static IEnumerable<Vector2> GetVectors(int cnt)
-        {
-            for (int i = 0; i < cnt; i++)
-            {
-                var y =
-                    i == 1 ? 5 :
-                    i == 2 ? 3 :
-                    1;
-                yield return new Vector2(i, y);
-            }
-        }
-        public static Bezier CreateTestBezier()
-        {
-            var pts = GetVectors(4).ToArray();
-            var x = new Bezier(pts[0], pts[1], pts[2], pts[3]);
-            return x;
-        }
-
-        public static Vector2 GetBezierValueAt(float t)
-        {
-            var pts = GetVectors(4).ToArray();
-            return new Vector2
-            {
-                X = (float)Solve(pts[0].X, pts[1].X, pts[2].X, pts[3].X, t),
-                Y = (float)Solve(pts[0].Y, pts[1].Y, pts[2].Y, pts[3].Y, t)
-            };
-            
-        }
-        public static double Solve(float p0, float p1, float p2, float p3, float t)
-        {
-            return Math.Pow(1 - t, 3f) * p0 + 3 * Math.Pow(1 - t, 2f) * t * p1 + 3 * (1 - t) * Math.Pow(t, 2) * p2 + Math.Pow(t, 3f) * p3;
-        }
-    }
-    public class Test
+    public class BezierTest
     {
         [Test]
-        public void test()
+        public void VisualizeTest()
         {
-            // creating a bezier curve
-            var curve = CurveFactory.CreateTestBezier();
+            // creating a bezier curve with library
+            var curve = BezierFactory.GetTestBezierWithLib();
 
-            // visualizing in test output
+            // ライブラリを利用したベジェ曲線をアウトプットログに可視化
+            Console.WriteLine("### Bezier with Library ###");
             for(float i = 0f; i < 1f; i += 0.05f)
             {
-                var y = curve.Position(i);
-                string bar = "|";
-                var cnt = 0f;
-                while(cnt < y.Y)
-                {
-                    bar += "|";
-                    cnt += 0.1f;
-                }
-                Console.WriteLine($"{string.Format("{0:0.0}",y.X)}:{bar}:({y.X}:{y.Y})");
+                var pos = curve.Position(i);
+                VisualizeForOutput(pos);
             }
 
+            // 自力実装のベジェ曲線をアウトプットログに可視化
+            Console.WriteLine("### Bezier from scratch ###");
             for (float i = 0f; i < 1f; i += 0.05f)
             {
-                var pos = CurveFactory.GetBezierValueAt(i);
-                string bar = "|";
-                var cnt = 0f;
-                while (cnt < pos.Y)
-                {
-                    bar += "|";
-                    cnt += 0.1f;
-                }
-                Console.WriteLine($"{string.Format("{0:0.0}", pos.X)}:{bar}({pos.X}:{pos.Y})");
+                var pos = BezierFactory.GetTestBezierValueAt(i);
+                VisualizeForOutput(pos);
             }
-            Assert.IsNotNull(curve);
+
+            // とりあえずt=0.5地点において両社の値が正しいことを確認
+            Assert.True(curve.Position(0.5f).Y == BezierFactory.GetTestBezierValueAt(0.5f).Y);
         }
+
+        /// <summary>
+        /// ベクトルを出力ログ向けの文字列として可視化します。
+        /// </summary>
+        /// <param name="pos"></param>
+        private void VisualizeForOutput(Vector2 pos)
+        {
+            string bar = "|";
+            var cnt = 0f;
+            // y の値になるまで.1刻みでバーを出力
+            while (cnt < pos.Y)
+            {
+                bar += "|";
+                cnt += 0.1f;
+            }
+            var x = FloatToString(pos.X);
+            var y = FloatToString(pos.Y);
+            Console.WriteLine($"{x}:{bar}({x}:{y})");
+        }
+
+        private string FloatToString(float x) => string.Format("{0:0.000}", x);
+
+
     }
 }
